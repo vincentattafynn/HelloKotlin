59ed689fb3b3ce0b1ae3289161f3b858f59e9dcd
vincentattafynn <vincentattafynn@gmail.com>
2023-09-22 07:50:27 +0000

    2nd kotlin commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,2 +1,9 @@
-class main {
+fun sayhello(greeting:String, itemToGreet:String) = println("Hello $itemToGreet")
+
+
+fun main(){
+    val interestingThings = arrayOf("Kotlin", "Programming", "Comicbooks", 1)
+    val map = mapOf(1 to "a", 2 to "b", 3 to "c")
+    map.forEach{key, value -> println("$key -> $value")}
+
 }
